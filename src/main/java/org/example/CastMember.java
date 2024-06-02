f006d33cf7b986ec4bc2de0c80bf1835e60bdba8
Julia Garbera <278443@student.pwr.edu.pl>
2024-06-02 19:03:32 +0200

    short description of changes

---
+++ src/main/java/org/example/CastMember.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class CastMember {
+    private String actorName;
+    private String characterName;
+
+    public CastMember(String actorName, String characterName) {
+        this.actorName = actorName;
+        this.characterName = characterName;
+    }
+
+    public String getActorName() {
+        return actorName;
+    }
+
+    public void setActorName(String actorName) {
+        this.actorName = actorName;
+    }
+
+    public String getCharacterName() {
+        return characterName;
+    }
+
+    public void setCharacterName(String characterName) {
+        this.characterName = characterName;
+    }
+
+    @Override
+    public String toString() {
+        return "CastMember{" +
+                "actorName='" + actorName + '\'' +
+                ", characterName='" + characterName + '\'' +
+                '}';
+    }
+}
