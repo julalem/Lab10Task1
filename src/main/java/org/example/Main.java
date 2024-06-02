1ae2f48b7e0f7a0896cd835d2d55bb7be67ee414
Julia Garbera <278443@student.pwr.edu.pl>
2024-06-02 19:13:06 +0200

    short description of changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,38 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws IOException{
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        List<CastMember> cast1 = new ArrayList<>();
+        cast1.add(new CastMember("Tom Hanks", "Forrest Gump"));
+        cast1.add(new CastMember("Robin Wright", "Jenny Curran"));
+
+        List<CastMember> cast2 = new ArrayList<>();
+        cast2.add(new CastMember("Joanna Kulig", "Zula Lichoń"));
+        cast2.add(new CastMember("Tomasz Kot", "Wiktor Warski"));
+
+
+        Movie movie1 = new Movie("Forrest Gump", "Robert Zemeckis",
+                142, cast1);
+        Movie movie2 = new Movie("Zimna wojna", "Paweł Pawlikowski",
+                88, cast2);
+
+        List<Movie> movies = new ArrayList<>();
+        movies.add(movie1);
+        movies.add(movie2);
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(movies);
+        
+        System.out.println(json);
     }
 }
